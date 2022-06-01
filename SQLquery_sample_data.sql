fb5c29745607bd2eef6f4cebac0425e7c617b103
k-lakshmikanth <94531416+k-lakshmikanth@users.noreply.github.com>
2022-06-01 13:32:45 +0530

    Add files via upload

---
+++ SQLquery_sample_data.sql
@@ -0,0 +1,59 @@
+create table sample_data(
+time VARCHAR(1000),
+variation_name_1 VARCHAR(1000),
+variation_api_id_1 VARCHAR(1000),
+sent_1 VARCHAR(1000),
+opens_1 VARCHAR(1000),
+machine_open_1 VARCHAR(1000),
+unique_opens_1 VARCHAR(1000),
+machine_amp_open_1 VARCHAR(1000),
+clicks_1 VARCHAR(1000),
+unique_clicks_1 VARCHAR(1000),
+unsubscribers_1 VARCHAR(1000),
+bounces_1 VARCHAR(1000),
+delivered_1 VARCHAR(1000),
+reported_spam_1 VARCHAR(1000),
+revenue_1 VARCHAR(1000),
+unique_recipients_1 VARCHAR(1000),
+conversions_1 VARCHAR(1000),
+conversions_by_send_time_1 VARCHAR(1000),
+conversions1_1 VARCHAR(1000),
+conversions1_by_send_time_1 VARCHAR(1000),
+conversions2_1 VARCHAR(1000),
+conversions2_by_send_time_1 VARCHAR(1000),
+conversions3_1 VARCHAR(1000),
+conversions3_by_send_time_1 VARCHAR(1000),
+variation_name_2 VARCHAR(1000),
+variation_api_id_2 VARCHAR(1000),
+enrolled_2 VARCHAR(1000),
+revenue_2 VARCHAR(1000),
+unique_recipients_2 VARCHAR(1000),
+conversions_2 VARCHAR(1000),
+conversions_by_send_time_2 VARCHAR(1000),
+conversions1_2 VARCHAR(1000),
+conversions1_by_send_time_2 VARCHAR(1000),
+conversions2_2 VARCHAR(1000),
+conversions2_by_send_time_2 VARCHAR(1000),
+conversions3_2 VARCHAR(1000),
+conversions3_by_send_time_2 VARCHAR(1000),
+conversions_by_send_time VARCHAR(1000),
+conversions1_by_send_time VARCHAR(1000),
+conversions2_by_send_time VARCHAR(1000),
+conversions3_by_send_time VARCHAR(1000),
+conversions VARCHAR(1000),
+conversions1 VARCHAR(1000),
+conversions2 VARCHAR(1000),
+conversions3 VARCHAR(1000),
+unique_recipients VARCHAR(1000),
+revenue VARCHAR(1000),
+message VARCHAR(1000));
+
+
+select * from sample_data
+
+DROP TABLE sample_data
+
+
+TRUNCATE TABLE sample_data
+
+
